11b2d979dc4eef488ccdcbe1fc8c3c8e818797ac
Mariana Castañeda Bedoya <93689558+Mrcsbda@users.noreply.github.com>
2026-07-23 17:45:10 -0500

    feat: change pnpm to yarn

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -6,18 +6,30 @@ type SuccessCallback = () => void;
 type ErrorCallback = (error: string) => void;
 
 export class CheckService implements ICheckServiceUseCase {
+    // private successCallback: SuccessCallback;
+    // private errorCallback: ErrorCallback;
+
+    // constructor(
+    //     successCallback: SuccessCallback,
+    //     errorCallback: ErrorCallback
+    // ) {
+    //     this.successCallback = successCallback;
+    //     this.errorCallback = errorCallback;
+    // }
 
     constructor(
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback
-    ) { }
+    ) {
+
+    }
 
     async execute(url: string): Promise<boolean> {
 
         try {
             const response = await fetch(url);
             if (!response.ok) {
-                throw new Error(`Error on check service ${url}: ${response.statusText}`);
+                throw new Error(`Error on check service ${url}: ${response.statusText}`); // se hace esto para que el error sea capturado en el catch y se ejecute el callback de error
             }
             this.successCallback();
             return true
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -9,13 +9,11 @@ export class Server {
         const onTick = () => {
 
             const url = "https://www.google.com";
+            // const url = "http://localhost:3000/posts";
+            const checkServiceSuccess = () => console.log(`Success callback called, service ${url} is up`)
+            const checkServiceError = (error: any) => console.log(`Error callback called with error: ${error}`)
 
-            new CheckService(
-                () => console.log(`Success callback called, service ${url} is up`),
-                (error) => console.log(`Error callback called with error: ${error}`)
-            )
-                .execute(url);
-            // new CheckService().execute("http://localhost:3000/posts")
+            new CheckService(checkServiceSuccess, checkServiceError).execute(url);
         };
 
         CronService.createJob(cronTime, onTick);
